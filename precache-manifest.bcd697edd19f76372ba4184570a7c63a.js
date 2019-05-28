d24837705fff66ec0b04df99339e48e8549acc53
MigueMartelo <miguemartelo1031@gmail.com>
2019-05-28 12:47:46 -0500

    Updates

---
+++ precache-manifest.bcd697edd19f76372ba4184570a7c63a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb204af60f14765d516c8781645f636",
+    "url": "/youtubevideoApp/index.html"
+  },
+  {
+    "revision": "4c36a8a8010b153e8084",
+    "url": "/youtubevideoApp/static/css/main.28d4d2bb.chunk.css"
+  },
+  {
+    "revision": "5e6000cea1873ac72c3e",
+    "url": "/youtubevideoApp/static/js/2.2ae674de.chunk.js"
+  },
+  {
+    "revision": "4c36a8a8010b153e8084",
+    "url": "/youtubevideoApp/static/js/main.eb88c161.chunk.js"
+  },
+  {
+    "revision": "591be566cbce709d04bc",
+    "url": "/youtubevideoApp/static/js/runtime~main.8b8d8a9e.js"
+  }
+]);
